4e31a902abe6560a85878e48206a29d098693efb
Mikhail Kudryashev <uamedwed@gmail.com>
2023-12-19 19:05:38 +0100

    fix: remove unused function

--- src/api/users/users.interface.ts
+++ src/api/users/users.interface.ts
@@ -3,10 +3,6 @@ import BaseInterface from '../../system/base/base.interface'
 
 export default class UsersInterface extends BaseInterface {
 	private readonly _schema: any
-	private readonly userLinks = this.Rules.object().keys({
-		name: this.Rules.string(),
-		link: this.Rules.string()
-	})
 
 	constructor() {
 		super()
